00ae22d69c3b48cb0f0b618041f4272b90c1d474
WillMay09 <wmayhood@outlook.com>
2024-03-20 10:18:14 -0400

    populated the content div with the correct amount of grid-divs
    Having the correct amount of grid-divs that react to the cursor create a proper looking grid that can be colored in

--- implementation.js
+++ implementation.js
@@ -2,27 +2,65 @@
 
 /*populating the content div with smaller divs that will serve as the blocks in which
   the user can color*/
-function populateGrid(){
 
+  let GRID_WIDTH = 800; //width of the drawing board
+  let GRID_HEIGHT = 800;
+//default number of squares is 16
+function populateGrid(squareNumber = 16){
+
+let sizeOfSquare = GRID_WIDTH/squareNumber;
+let area = squareNumber*squareNumber;
 const contentDiv = document.querySelector(".content");
 
-for(let i=0; i<256;i++){
+for(let i=0; i<area;i++){
 
 let newDiv = document.createElement('div');
 newDiv.classList.add('grid-div');
+newDiv.style.width = sizeOfSquare + "px";
+newDiv.style.height = sizeOfSquare + "px";
 contentDiv.appendChild(newDiv);
 
 }
+
+addHoverEffect();
+
+}
+
 //adds hovered css class to each div, causing them to change color, acting like a marker
-let hoverEffect =document.querySelectorAll('.grid-div');
-hoverEffect.forEach(function(hoverEffect){
+function addHoverEffect(){
 
-hoverEffect.addEventListener('mouseover', function(){
+  let hoverEffect =document.querySelectorAll('.grid-div');
+  hoverEffect.forEach(function(hoverEffect){
+  
+  hoverEffect.addEventListener('mouseover', function(){
+  
+    hoverEffect.classList.add('hovered');
+  })
+  
+  })
 
-  hoverEffect.classList.add('hovered');
-})
 
-})
+}
+
+
+function calculateGridSize(){
+  let generateButton = document.querySelector('.squareSize')
+  
+
+  generateButton.addEventListener('click',function(){
+
+    let textField = document.querySelector('.numberOfSquares').textContent;
+    populateGrid(textField);
+   
+    
+    //Calc div size
+
+
+
+  })
+
+  //console.log(textField);
+
 
 }
 
@@ -31,7 +69,7 @@ hoverEffect.addEventListener('mouseover', function(){
 function playGame(){
 
     populateGrid();
-
+    calculateGridSize();
 
 
 }
